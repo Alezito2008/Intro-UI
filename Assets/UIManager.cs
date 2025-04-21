272677d12a5d109081429b8fe140755279b5a800
Alezito2008 <>
2025-04-21 10:40:28 -0300

    saluda

---
+++ Assets/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+
+    string textoInicial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textoInicial = txtSaludo.text;
+        txtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        string nombre = inputNombre.text;
+        txtSaludo.text = string.Format(textoInicial, nombre);
+    }
+}
